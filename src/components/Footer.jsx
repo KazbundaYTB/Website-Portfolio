dcec2a404f86c1c9ad3ee0edcca7a7ef708f990f
R-Pavlik <gejmraopi11@gmail.com>
2024-07-18 20:44:26 +0200

    Deleted old components.

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div className='flex justify-center items-center h-full'>
-      <h1 className='bg-transparent text-white rounded-full px-4 py-2 font-bold flex items-center gap-2'> © 2024 - ReactPortfolio v1.0 - Made by Kazbunda with ❤️ </h1>
-    </div>
-  )
-}
