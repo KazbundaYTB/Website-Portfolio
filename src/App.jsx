823f2befe2525913ca98f9618790814e4b3cb4f3
R-Pavlik <gejmraopi11@gmail.com>
2024-07-18 23:57:31 +0200

    Recreated AboutMe.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
 import './index.css';
-import AboutMe from "./components/AboutMe"
-import NavBar from "./components/NavBar"
-import Portfolio from "./components/Portfolio"
-import Footer from "./components/Footer"
-
-function App() {
+import AboutMe from './components/AboutMe';
+export default function App() {
   return (
-    <div className="App">
-      <div className='w-full h-[85px] bg-gray-800 align-middle justify-center'>
-        <NavBar />
-      </div>
-
-      <div className='w-full h-[800px] bg-gray-700 flex align-middle justify-center'>
+    <div className='w-screen h-screen bg-slate-900 overflow-x-hidden'>
         <AboutMe />
-      </div>
-      <div className='w-full h-[800px] bg-gray-700 flex align-middle justify-center'>
-        <Portfolio />
-      </div>
-      
-      <div className='w-full h-[85px] bg-gray-600'>
-        <Footer />
-      </div>
+        <div className='w-screen h-[10%]'></div>
     </div>
   );
 }
-
-export default App;
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,42 +1,54 @@
-import React from 'react'
-import ProfileImage from '../img/profile.png';
-import ProfileBanner from '../img/banner.gif';
-import { FaInstagram, FaGithub, FaDiscord } from "react-icons/fa";
+import { React, useState, useEffect } from "react";
+import Memoji from "../img/1.png";
+import Background from "../img/background.jpg";
+import { FaDiscord } from "react-icons/fa";
 
 export default function AboutMe() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-      <div className='w-[1200px] h-[750px] rounded-xl bg-gray-900 mt-6 mb-6 translate-y-9' id='about'>
-        <div className='w-full h-[300px] bg-white rounded-xl relative'>
-          <img src={ProfileBanner} alt="Profile" className="w-[1200px] h-[300px] object-cover absolute rounded-xl"/>
+    <div className={`w-full h-full ${windowWidth <= 980 ? 'overflow-y-green' : 'overflow-y-blue'}`}>
+      <div
+        className={`w-full h-[75%] flex ${
+          windowWidth <= 980
+            ? "flex-col w-full h-full justify-center items-center"
+            : "flex-row w-full h-full justify-around items-center"
+        }`}
+        style={{
+          backgroundImage: `url(${Background})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div
+          className={`${
+            windowWidth <= 980
+              ? "flex justify-center items-center flex-col text-center"
+              : "flex justify-center items-start flex-col"
+          }`}
+        >
+          <h1 className="text-violet-400 text-3xl font-bold">WEBSITE PROGRAMATOR</h1>
+          <h1 className="text-white text-6xl font-mono font-bold">Richard Pavlík</h1>
+          <a href="https://discordapp.com/users/714505079163715625">
+            <button className="text-white px-4 py-2 bg-indigo-600 rounded-2xl flex flex-row">
+              <FaDiscord className="mt-1 mr-2" /> Kazbunda#9069
+            </button>
+          </a>
         </div>
-        <div className='flex flex-row space-x-6'>
-          <div className='w-[220px] h-[220px] rounded-full bg-green-800 -translate-y-28 translate-x-6 flex items-center justify-center overflow-hidden relative'>
-            <img src={ProfileImage} alt="Profile" className="w-full h-full object-cover absolute transform scale-100"/>
-          </div>
-          <div className='flex flex-col -space-y-12'>
-            <div className='w-[320px] h-[45px] bg-gray-900 rounded-xl mt-6 mb-4 flex items-center justify-center font-bold text-white text-xl -translate-y-12 translate-x-6 gap-6'>
-              <h1>Kazbunda</h1>
-
-              <button> <a href="https://www.instagram.com/pavlik._.r/"><FaInstagram className="hover:text-green-400" style={{ fontSize: '135%' }}/></a></button>
-              <button><a href="https://github.com/KazbundaYTB/"><FaGithub className="hover:text-green-400" style={{ fontSize: '135%' }}/></a></button>
-              <button><a href="https://discordapp.com/users/714505079163715625"><FaDiscord className="hover:text-green-400" style={{ fontSize: '135%' }}/></a></button>
-            </div>
-            <div className='w-[320px] h-[30px] bg-gray-800 rounded-xl mt-6 mb-4 flex items-center justify-center text-white text-xl -translate-y-2 translate-x-6'>
-              <h1>Floorball goalie - #23</h1>
-            </div>
-          </div>
-        </div>
-        <div className='w-[1150px] h-[280px] rounded-xl translate-x-6 -translate-y-20 bg-gray-700'>
-          <div className='w-1/2 h-full bg-transparent rounded-xl text-white'>
-            <h1 className='translate-y-6 translate-x-6 text-xl'> About me:</h1>
-            <div className='w-[90px] h-[2px] bg-white translate-y-6 translate-x-6'></div>
-
-            <h1 className='translate-y-8 translate-x-8 text-xl'> » 👑 - Majitel: InsanePlay.eu</h1>
-            <h1 className='translate-y-8 translate-x-8 text-xl'> » 🥅 - Floorball Goalie - #23</h1>
-            <h1 className='translate-y-8 translate-x-8 text-xl'> » 👨‍💻 -  React.JS, TailwindCSS</h1>
-            
-          </div>
-        </div>
+        <img src={Memoji} alt="" />
       </div>
-  )
+    </div>
+  );
 }
